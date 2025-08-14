11de0f2de8d89bb66ee0268c07202cc12d44b26e
Ismael Pastor <ispaluan@gmail.com>
2025-08-14 20:22:49 +0200

    Solucionados bugs de la Semana 1

--- Documents/DESARROLLO/weatherapp-pro/src/App.jsx
+++ Documents/DESARROLLO/weatherapp-pro/src/App.jsx
@@ -5,15 +5,25 @@ import WeatherCard from './components/WeatherCard';
 
 export default function App() {
   const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState('');
 
   const fetchWeather = async (city) => {
     try {
       const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
       const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=es`;
       const response = await axios.get(url);
-    } catch (error) {
-      console.error('Error al obtener datos del clima:', error);
+
+      if(response.data.error) {
+        setError('Ciudad no Encontrada.');
+        setWeatherData(null);
+      } else {
+        setWeatherData(response.data);
+        setError('');
+      }
+    } catch (err) {
+      console.error('Error al obtener datos del clima:', err);
       setWeatherData(null);
+      setError('Error al conectar con WeatherAPI')
     }
   };
 
@@ -22,6 +32,7 @@ export default function App() {
     <div>
       <h1>WeatherApp Pro</h1>
       <SearchBar onSearch={fetchWeather} />
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       {weatherData && <WeatherCard data={weatherData} />}    
     </div>
   )   
